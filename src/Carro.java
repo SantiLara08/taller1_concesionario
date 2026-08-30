6fe033360d60525e3993b3c0fc26584f7b8169cb
Santiago Lara <slarac@icloud.com>
2026-08-30 15:03:49 -0500

    Organiacion de metodo

--- src/Carro.java
+++ src/Carro.java
@@ -3,8 +3,7 @@ public class Carro extends Vehiculo {
     private final int numeroPuertas;
     private String tipoTransmision;
 
-    public Carro(String placa, String marca, String modelo, double tarifaDia,
-                 int numeroPuertas, String tipoTransmision) {
+    public Carro(String placa, String marca, String modelo, double tarifaDia, int numeroPuertas, String tipoTransmision) {
         super(placa, marca, modelo, tarifaDia);
         this.numeroPuertas = numeroPuertas;
         this.tipoTransmision = tipoTransmision;
