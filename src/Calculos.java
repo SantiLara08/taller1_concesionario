188b0e514ff4e44e8c690565fdcb077081f5fdc2
AJ-34-SO <andresantiagojaimes@gmail.com>
2026-08-30 13:34:12 -0500

    Create Calculos.java

---
+++ src/Calculos.java
@@ -0,0 +1,11 @@
+```java
+public class Calculos {
+
+    private Calculos() {
+    }
+
+    public static double calcularDescuento(double monto, double porcentaje) {
+        return monto - (monto * porcentaje);
+    }
+}
+```
